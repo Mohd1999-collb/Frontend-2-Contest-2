e2f44cb565b3c4461bcb6bd6a6e054d88384fef9
Mohd <talibsidaug1999@gmail.com>
2023-08-19 14:29:11 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const elements = document.getElementsByClassName("student");
+let button = document.getElementsByTagName("button");
+// console.log(elements[0].value);
+// console.log(button);
+
+
+
+
+
